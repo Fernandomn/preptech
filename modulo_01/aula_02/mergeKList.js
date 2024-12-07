935617d0c022ac478e35b53fc93e1e9ba3b8252f
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2024-12-07 13:59:19 -0300

    finishing mergeKsort

--- modulo_01/aula_02/mergeKList.js
+++ modulo_01/aula_02/mergeKList.js
@@ -1,9 +1,11 @@
 /**
+ * https://leetcode.com/problems/merge-k-sorted-lists/description/
+ * https://leetcode.com/problems/merge-k-sorted-lists/solutions/6123434/brute-force-was-not-cool/
  * 23. Merge k Sorted Lists
  * You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
- * 
+ *
  * Merge all the linked-lists into one sorted linked-list and return it.
- * 
+ *
  * Example 1:
  * Input: lists = [[1,4,5],[1,3,4],[2,6]]
  * Output: [1,1,2,3,4,4,5,6]
@@ -13,18 +15,18 @@
  *   1->3->4,
  *   2->6
  * ]
- * 
+ *
  * merging them into one sorted list:
  * 1->1->2->3->4->4->5->6
- * 
+ *
  * Example 2:
  * Input: lists = []
  * Output: []
- * 
+ *
  * Example 3:
  * Input: lists = [[]]
  * Output: []
- * 
+ *
  * Constraints:
  * k == lists.length
  * 0 <= k <= 104
@@ -45,6 +47,56 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-var mergeKLists = function(lists) {
-    
+function mergeKLists(lists) {
+  // // Using Bruteforce
+  // let remainingLists: number = lists.reduce((acc, cur) => acc + (cur != null ? 1 : 0), 0);
+
+  // let listHeader = new ListNode();
+  // let current = listHeader
+
+  // while (remainingLists) {
+  //     let nextListIndex = -1
+  //     let minValue = Infinity
+
+  //     for (let i = 0; i < lists.length; i++) {
+  //         if (lists[i]) {
+  //             if (lists[i].val < minValue) {
+  //                 minValue = lists[i].val
+  //                 nextListIndex = i
+  //             }
+  //         }
+  //     }
+  //     current.next = lists[nextListIndex]
+  //     lists[nextListIndex] = lists[nextListIndex].next
+  //     if (!lists[nextListIndex]) remainingLists--
+  //     current = current.next
+  // }
+
+  // return listHeader.next
+
+  let values = [];
+
+  for (let list of lists) {
+    let current = list;
+    while (current) {
+      values.push(current.val);
+      current = current.next;
+    }
+  }
+
+  values.sort((a, b) => a - b);
+  return arrayToList(values);
+}
+
+const arrayToList = (values) => {
+  if (values.length == 0) return null;
+
+  const newList = new ListNode();
+  let current = newList;
+  for (let value of values) {
+    current.next = new ListNode(value);
+    current = current.next;
+  }
+
+  return newList.next;
 };
