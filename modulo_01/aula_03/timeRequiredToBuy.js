2ad9f60606846aa8bc066a2730d88fa4cfcdba75
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2024-12-26 20:54:07 -0300

    adding queueSolution on timeRequiredToBuy

--- modulo_01/aula_03/timeRequiredToBuy.js
+++ modulo_01/aula_03/timeRequiredToBuy.js
@@ -4,27 +4,54 @@
  * @return {number}
  */
 var timeRequiredToBuy = function (tickets, k) {
-  // const fila = [];
+  // return mathSolution(tickets, k)
+
+  return queueSolution(tickets, k)
+};
+
+const mathSolution = (tickets, k) => {
   let time = 0;
   let qnt = tickets[k];
 
   for (let i = 0; i < tickets.length; i++) {
-    if (i < k) {
-      if (tickets[i] > qnt) {
-        time += qnt;
+      if (i < k) {
+          if (tickets[i] > qnt) {
+              time += qnt
+          } else {
+              time += tickets[i]
+          }
+      } else if (i > k) {
+          if (tickets[i] >= qnt) {
+              time += qnt - 1;
+          } else {
+              time += tickets[i]
+          }
       } else {
-        time += tickets[i];
+          time += qnt;
       }
-    } else if (i > k) {
-      if (tickets[i] >= qnt) {
-        time += qnt - 1;
-      } else {
-        time += tickets[i];
-      }
-    } else {
-      time += qnt;
-    }
   }
 
   return time;
-};
+}
+const queueSolution = (tickets, k) => {
+  // NOTE: it`s failing for some cases
+  let time = 0
+
+  while (tickets.length > 0) {
+      time++
+      console.log("time:", time)
+      // console.log("loop:", time)
+
+      console.log("tickets:", tickets)
+      let current = tickets.shift()
+
+      current--
+
+      if (current > 0) {
+          tickets.push(current)
+      }
+
+      console.log("Updated tickets:", tickets)
+  }
+  return time
+}
