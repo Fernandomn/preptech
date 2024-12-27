b2f8a63d4a6b4f3758460a8b4a17fbbf3bcbf47d
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2024-12-27 00:46:52 -0300

    adding mergeSort

--- modulo_01/aula_04/mergeSort.js
+++ modulo_01/aula_04/mergeSort.js
@@ -1,16 +1,48 @@
-const mergeSort = (nums, start, end) => {
+const mergeSort = (arr, start, end) => {
   if (start >= end) return;
 
   const mid = Math.floor(start + (end - start) / 2);
-  mergeSort(nums, start, mid);
-  mergeSort(nums, mid + 1, end);
+  mergeSort(arr, start, mid);
+  mergeSort(arr, mid + 1, end);
   merge(arr, start, mid, end);
 };
 
 const merge = (arr, start, mid, end) => {
+  let n1 = mid - start + 1;
+  let n2 = end - mid;
   const left = [];
   const right = [];
 
-  // for(let i = 0; i < )
-};
+  for (let i = 0; i < n1; i++) {
+    left[i] = arr[start + i];
+  }
+  for (let j = 0; j < n2; j++) {
+    right[j] = arr[mid + 1 + j];
+  }
 
+  let i = 0,
+    j = 0;
+  let k = start;
+
+  while (i < n1 && j < n2) {
+    if (left[i] <= right[j]) {
+      arr[k] = left[i];
+      i++;
+    } else {
+      arr[k] = right[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < n1) {
+    arr[k] = left[i];
+    i++;
+    k++;
+  }
+  while (j < n2) {
+    arr[k] = right[j];
+    j++;
+    k++;
+  }
+};
