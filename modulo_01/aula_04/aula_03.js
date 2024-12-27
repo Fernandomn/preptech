c739d0246b3b883954c9188b7ccd1b61ae02d0d6
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2024-12-26 22:21:45 -0300

    Updating bubbleSort

--- modulo_01/aula_04/aula_03.js
+++ modulo_01/aula_04/aula_03.js
@@ -13,7 +13,16 @@ const selectionSort = (nums) => {
 };
 
 const bubbleSort = (nums) => {
-  return nums;
+  for (let i = nums.length - 1; i > 1; i--) {
+    let anySwap = false;
+    for (let j = 1; j < i; j++) {
+      if (nums[j] > nums[j + 1]) {
+        [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
+        anySwap = true;
+      }
+    }
+    if (!anySwap) break;
+  }
 };
 
 const mergeSort = (nums, start, end) => {
