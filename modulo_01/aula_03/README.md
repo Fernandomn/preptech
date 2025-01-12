bb65d4bbf6c25338b77eefaf3c40dfb143e84634
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2025-01-11 21:00:17 -0300

    Updating README of moddulo_01/caula_03, and some files

--- modulo_01/aula_03/README.md
+++ modulo_01/aula_03/README.md
@@ -1,2 +1,9 @@
 # Módulo 01 - Aula 03
 ## Pilhas e Filas
+
+Conteúdo do módulo:
+- 150. [Evaluate Reverse Polish Notation](https://leetcode.com/problems/evaluate-reverse-polish-notation/description/)
+- 1047. [Remove All Adjacent Duplicates In String](https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/)
+- 1700. [Number of Students Unable to Eat Lunch](https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/)
+- 2073. [Time Needed to Buy Tickets](https://leetcode.com/problems/time-needed-to-buy-tickets/description/)
+- 2785. [Sort Vowels in a String](https://leetcode.com/problems/sort-vowels-in-a-string/description/)
--- modulo_01/aula_03/sortVowel.js
+++ modulo_01/aula_03/sortVowel.js
@@ -1,31 +1,48 @@
+/**
+ * https://leetcode.com/problems/sort-vowels-in-a-string/description/
+ * https://leetcode.com/problems/sort-vowels-in-a-string/solutions/6266948/wee-need-to-sort-it-right-by-fernamn-5rcf/
+ * 2785. Sort Vowels in a String
+Given a 0-indexed string s, permute s to get a new string t such that:
+
+All consonants remain in their original places. More formally, if there is an index i with 0 <= i < s.length such that s[i] is a consonant, then t[i] = s[i].
+The vowels must be sorted in the nondecreasing order of their ASCII values. More formally, for pairs of indices i, j with 0 <= i < j < s.length such that s[i] and s[j] are vowels, then t[i] must not have a higher ASCII value than t[j].
+Return the resulting string.
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in lowercase or uppercase. Consonants comprise all letters that are not vowels.
+
+
+Example 1:
+Input: s = "lEetcOde"
+Output: "lEOtcede"
+Explanation: 'E', 'O', and 'e' are the vowels in s; 'l', 't', 'c', and 'd' are all consonants. The vowels are sorted according to their ASCII values, and the consonants remain in the same places.
+
+Example 2:
+Input: s = "lYmpH"
+Output: "lYmpH"
+Explanation: There are no vowels in s (all characters in s are consonants), so we return "lYmpH".
+ 
+
+Constraints:
+1 <= s.length <= 105
+s consists only of letters of the English alphabet in uppercase and lowercase.} s 
+ */
+
 function sortVowels(s) {
-  const indexList = [];
   const vowelStack = [];
+  const vowelSet = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
   let splittedString = s.split('');
 
   for (let i = 0; i < s.length; i++) {
-    if (isVowel(s[i])) {
-      indexList.push(i);
+    if (vowelSet.has(s[i])) {
       vowelStack.push(s[i]);
     }
   }
 
-  vowelStack.sort(vowelSort);
+  vowelStack.sort().reverse();
 
   for (let j = 0; j < splittedString.length; j++) {
-    if (isVowel(splittedString[j])) {
+    if (vowelSet.has(splittedString[j])) {
       splittedString[j] = vowelStack.pop();
     }
   }
   return splittedString.join('');
 }
-
-const isVowel = (character) => {
-  return (
-    ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'].indexOf(character) >= 0
-  );
-};
-
-const vowelSort = (a, b) => {
-  return a < b ? 1 : a > b ? -1 : 0;
-};
