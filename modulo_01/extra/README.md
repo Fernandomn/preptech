d489cb759606441f8bb5f869f4c5eedfb5190a5b
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2025-01-26 16:04:33 -0300

    Adding hasCycle

--- modulo_01/extra/README.md
+++ modulo_01/extra/README.md
@@ -3,5 +3,6 @@
 
 Conteúdo do módulo:
 - [28. Find the Index of the First Occurrence in a String](https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/solutions/6321777/two-pointers-are-cool-but-you-just-need-21j5q/)
+- [141. Linked List Cycle](https://leetcode.com/problems/linked-list-cycle/solutions/6332871/fast-and-slow-by-fernamn-0hzd/)
 - [387. First Unique Character in a String](https://leetcode.com/problems/first-unique-character-in-a-string/solutions/6321526/not-hard-by-fernamn-xtgo/)
 - [1313. Decompress Run-Length Encoded List](https://leetcode.com/problems/decompress-run-length-encoded-list/solutions/6324157/its-pretty-straightforward-by-fernamn-wdh5/)
---
+++ modulo_01/extra/hasCycle.ts
@@ -0,0 +1,97 @@
+/**
+ * https://leetcode.com/problems/linked-list-cycle/description/
+ * https://leetcode.com/problems/linked-list-cycle/solutions/6332871/fast-and-slow-by-fernamn-0hzd/
+ * 
+ * 141. Linked List Cycle
+Solved
+Easy
+Topics
+Companies
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+ 
+
+Example 1:
+
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+Example 2:
+
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+Example 3:
+
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+ 
+
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
+ 
+
+Follow up: Can you solve it using O(1) (i.e. constant) memory?
+ */
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+  if (!head || !head.next) {
+    return false;
+  }
+
+  // return usingSet(head)
+  return usingFastSlow(head);
+}
+
+const usingSet = (head: ListNode | null): boolean => {
+  const set = new Set<ListNode>();
+
+  let current = head;
+
+  while (current) {
+    if (set.has(current)) {
+      return true;
+    }
+    set.add(current);
+    current = current.next;
+  }
+  return false;
+};
+
+const usingFastSlow = (head: ListNode | null): boolean => {
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head;
+
+  while (slow && fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+};
